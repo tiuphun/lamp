9991f5b604aa94f175d742bd581dccb3f64bceb5
tiuphun <--global>
2024-04-10 14:35:36 +0700

    Recreate SQL queries for server deployment

--- tieu_db.sql
+++ tieu_db.sql
@@ -1,30 +1,8 @@
--- phpMyAdmin SQL Dump
--- version 4.0.9
--- http://www.phpmyadmin.net
---
--- Host: 127.0.0.1
--- Generation Time: Mar 27, 2014 at 05:08 AM
--- Server version: 5.6.14
--- PHP Version: 5.5.6
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+07:00";
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-
---
--- Database: `first_db`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `post`
---
+CREATE DATABASE tieu_db;
+USE tieu_db;
 
 CREATE TABLE IF NOT EXISTS `post` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -33,27 +11,8 @@ CREATE TABLE IF NOT EXISTS `post` (
   `time_posted` time NOT NULL,
   `date_edited` varchar(30) NOT NULL,
   `time_edited` time NOT NULL,
-  `public` varchar(5) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=8 ;
-
---
--- Dumping data for table `post`
---
-
--- INSERT INTO `post` (`id`, `details`, `date_posted`, `time_posted`, `date_edited`, `time_edited`, `public`) VALUES
--- (2, 'Tuna', 'March 26, 2014', '13:48:18', '', '00:00:00', 'no'),
--- (3, 'Salad', 'March 27, 2014', '04:47:49', '', '00:00:00', 'yes'),
--- (4, 'Corn', 'March 27, 2014', '04:48:11', '', '00:00:00', 'no'),
--- (5, 'Pasta', 'March 27, 2014', '04:48:26', '', '00:00:00', 'yes'),
--- (6, 'Chicken', 'March 27, 2014', '04:49:23', '', '00:00:00', 'yes'),
--- (7, 'Spaghetti', 'March 27, 2014', '04:49:49', '', '00:00:00', 'no');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `users`
---
 
 CREATE TABLE IF NOT EXISTS `user` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -62,13 +21,6 @@ CREATE TABLE IF NOT EXISTS `user` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
 
---
--- Dumping data for table `users`
---
-
--- INSERT INTO `user` (`id`, `username`, `password`) VALUES
--- (1, 'xtian', '123456');
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+ALTER TABLE post
+ADD COLUMN user_id INT,
+ADD FOREIGN KEY (user_id) REFERENCES user(id);
